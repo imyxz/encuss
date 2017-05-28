b2873e4e2258b0cd970aa3b03221a370c55e6874
imyxz <i@yxz.me>
2017-05-29 00:42:50 +0800

    调整样式，支持嵌套回复

--- js/embed.js
+++ js/embed.js
@@ -10,49 +10,61 @@ var user_avatar;
 var isLogin=false;
 var encuss_sub_replybox;
 var encuss_allreplys=new Object();
-includeCss("http://localhost/encuss/css/embed.css");
-site_id=encussConfig.site_id;
-var encuss=document.getElementsByClassName("encuss-div")[0];
-site_post_id=encuss.dataset['postId'];
-var ajax=new XMLHttpRequest();
-ajax.open("GET","http://localhost/encuss/commentAPI/getPostReplys/site_id/" + site_id + "/site_post_id/" +site_post_id,true);
-ajax.onreadystatechange=function(){
-    if (ajax.readyState==4 && ajax.status==200)
-    {
-        var comment=document.createElement("div");
-        comment.className="encuss-comments";
-        var response=JSON.parse(ajax.responseText);
-        for(var index in response['replys'])
-        {
-            encuss_allreplys[response['replys'][index]['comment_id']]=response['replys'][index];
-        }
-        for(var index in response['replys'])
-        {
-            comment.appendChild(createCommentNode(response['replys'][index]));
-        }
-        post_id=response.post_id;
-        if(response.isLogin==true)
-        {
-            user_nickname=response.nickname;
-            user_avatar=response.avatar;
-            isLogin=true;
-        }
-        else
-        {
-            isLogin=false;
-        }
+encussInit();
+function encussInit()
+{
+    includeCss("http://localhost/encuss/css/embed.css");
+    site_id=encussConfig.site_id;
+    var encuss=document.getElementsByClassName("encuss-div")[0];
+    site_post_id=encuss.dataset['postId'];
+    var ajax=new XMLHttpRequest();
+    ajax.open("GET","http://localhost/encuss/commentAPI/getPostReplys/site_id/" + site_id + "/site_post_id/" +site_post_id,true);
+    ajax.onreadystatechange=function(){
+        if (ajax.readyState==4 && ajax.status==200) {
+            var comment = document.createElement("div");
+            comment.className = "encuss-comments";
+            var response = JSON.parse(ajax.responseText);
+            for (var index in response['replys']) {
+                encuss_allreplys[response['replys'][index]['comment_id']] = response['replys'][index];
+            }
+            for (var index in response['replys']) {
+                comment.appendChild(createCommentNode(response['replys'][index]));
+            }
+            if (encussConfig.is_nest)//嵌套回复处理
+            {
+                for(var index in encuss_allreplys)
+                {
+                    if(encuss_allreplys[index].parent_id>0)
+                    {
+                        encuss_allreplys[index].element.style.marginLeft="20px";
+                        encuss_allreplys[encuss_allreplys[index].parent_id].element.appendChild(encuss_allreplys[index].element);
+                    }
+                }
+            }
+            post_id=response.post_id;
+            if(response.isLogin==true)
+            {
+                user_nickname=response.nickname;
+                user_avatar=response.avatar;
+                isLogin=true;
+            }
+            else
+            {
+                isLogin=false;
+            }
 
-        encuss.appendChild(comment);
-        encuss.appendChild(createReplyBox(0));
-        encuss_comments=document.getElementsByClassName("encuss-div")[0].getElementsByClassName("encuss-comments")[0];
+            encuss.appendChild(comment);
+            encuss.appendChild(createReplyBox(0));
+            encuss_comments=document.getElementsByClassName("encuss-div")[0].getElementsByClassName("encuss-comments")[0];
 
-        encuss_sub_replybox=createReplyBox(0);
-        encuss_sub_replybox.style.marginLeft="65px";
+            encuss_sub_replybox=createReplyBox(0);
+            encuss_sub_replybox.style.marginLeft="20px";
 
-    }
-};
-ajax.send();
+        }
+    };
+    ajax.send();
 
+}
 function includeCss(url) {
     var link = document.createElement("link");
     link.rel = "stylesheet";
@@ -66,10 +78,9 @@ function createCommentNode(info)
     ele.className="encuss-comments-node";
     ele.innerHTML='<img class="encuss-avatar">\
         <span class="encuss-username"></span>\
-    <span class="encuss-comment-content">\
-    </span>\
-    <span class="encuss-comment-time">\
-</span>\
+    <span class="encuss-comment-content-reply"></span>\
+    <span class="encuss-comment-content"></span>\
+    <span class="encuss-comment-time"></span>\
 <span class="encuss-comment-action">\
     <a href="javascript:void(0)" onclick="moveRelpyBox(this)">回复</a>\
     <a href="#">顶</a>\
@@ -77,13 +88,16 @@ function createCommentNode(info)
     info.content_reply="";
     if(info.parent_id>0)
     {
-        info.content_reply="回复 @" + encuss_allreplys[info.parent_id].user_nickname + ": "+encuss_allreplys[info.parent_id].content +"\n\n";
+        info.content_reply="回复 @" + encuss_allreplys[info.parent_id].user_nickname + ": "+encuss_allreplys[info.parent_id].content ;
     }
     ele.getElementsByClassName("encuss-avatar")[0].src=info.user_avatar;
     ele.getElementsByClassName("encuss-username")[0].innerText=info.user_nickname;
-    ele.getElementsByClassName("encuss-comment-content")[0].innerText=info.content_reply+ info.content;
+    if (!encussConfig.is_nest)
+        ele.getElementsByClassName("encuss-comment-content-reply")[0].innerText=info.content_reply;
+    ele.getElementsByClassName("encuss-comment-content")[0].innerText=info.content;
     ele.getElementsByClassName("encuss-comment-time")[0].innerText=info.time;
     ele.dataset.comment_id=info.comment_id;
+    encuss_allreplys[info.comment_id].element=ele;
     return ele;
 }
 function createReplyBox(parent_id)
@@ -106,11 +120,11 @@ function submitComment(curObj)
     var replyboxNode=curObj.parentNode.parentNode;
     var parent_id=replyboxNode.dataset['parent_id'];
     var textarea=replyboxNode.getElementsByClassName("encuss-replybox-area")[0].getElementsByTagName("textarea")[0];
-    if(postComment(parent_id,textarea.value))
+    if(postComment(parent_id,textarea.value,replyboxNode))
         textarea.value="";
 
 }
-function postComment(parent_id,content)
+function postComment(parent_id,content,replyboxNode)
 {
     var ajax=new XMLHttpRequest();
     var json_obj=new Object();
@@ -139,9 +153,28 @@ function postComment(parent_id,content)
                 info.time= d.toLocaleString();
                 encuss_allreplys[response.comment_id]=info;
                 var new_comment=createCommentNode(info);
-                encuss_comments.appendChild(new_comment);
-                window.scrollTo(0,new_comment.offsetTop);
+                if(parent_id>0 && encussConfig.is_nest==true)
+                {
+                    new_comment.style.marginLeft="20px";
+                    encuss_allreplys[parent_id].element.appendChild(new_comment);
+                }
+                else
+                {
+                    encuss_comments.appendChild(new_comment);
+                    window.scrollTo(0,new_comment.offsetTop-50);
+                }
 
+                encuss_allreplys[response.comment_id].element=new_comment;
+
+                if(replyboxNode!=null)
+                {
+                    var textarea=replyboxNode.getElementsByClassName("encuss-replybox-area")[0].getElementsByTagName("textarea")[0];
+                    console.log(textarea);
+                    textarea.value="";
+                    if(parent_id>0)
+                        replyboxNode.style.display="none";
+
+                }
                 return true;
             }
         }
@@ -153,5 +186,9 @@ function moveRelpyBox(curObj)
     var commentNode=curObj.parentNode.parentNode;
     encuss_sub_replybox.dataset['parent_id']=commentNode.dataset['comment_id'];
     encuss_sub_replybox.getElementsByClassName("encuss-replybox-area")[0].getElementsByTagName("textarea")[0].placeholder="回复 @" + encuss_allreplys[commentNode.dataset['comment_id']].user_nickname+":";
-    commentNode.appendChild(encuss_sub_replybox);
+    if(commentNode.getElementsByClassName("encuss-comments-node").length==0)//子评论
+        commentNode.appendChild(encuss_sub_replybox);
+    else
+        commentNode.insertBefore(encuss_sub_replybox,commentNode.getElementsByClassName("encuss-comments-node")[0])//保证刚好插在下面
+    encuss_sub_replybox.style.display="block";
 }
